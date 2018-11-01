8c1c792b662a9ac0a78ff1b18307860d22b86808
zzq <191550636@qq.com>
2018-11-01 17:21:04 +0800

    增加非selective的批量增加

--- src/main/java/com/kejso/user/generator/plugin/BatchInsertPlugin.java
+++ src/main/java/com/kejso/user/generator/plugin/BatchInsertPlugin.java
@@ -35,8 +35,54 @@ public class BatchInsertPlugin extends PluginAdapter{
   @Override
   public boolean sqlMapDocumentGenerated(Document document, IntrospectedTable introspectedTable) {
     addBatchInsertSelectiveXml(document, introspectedTable);
+    addBatchInsertXml(document, introspectedTable);
     return super.sqlMapDocumentGenerated(document, introspectedTable);
   }
+
+  private void addBatchInsertXml(Document document, IntrospectedTable introspectedTable) {
+    List<IntrospectedColumn> columns = introspectedTable.getAllColumns();
+    //获得要自增的列名
+    String incrementField = introspectedTable.getTableConfiguration().getProperties().getProperty("incrementField");
+    if(incrementField!=null){
+      incrementField = incrementField.toUpperCase();
+    }
+    XmlElement javaPropertyAndDbType = new XmlElement("trim");
+    javaPropertyAndDbType.addAttribute(new Attribute("prefix", " ("));
+    javaPropertyAndDbType.addAttribute(new Attribute("suffix", ")"));
+    javaPropertyAndDbType.addAttribute(new Attribute("suffixOverrides", ","));
+
+    XmlElement insertBatchElement = new XmlElement("insert");
+    insertBatchElement.addAttribute(new Attribute("id", "insertBatch"));
+    insertBatchElement.addAttribute(new Attribute("parameterType", "java.util.List"));
+
+    XmlElement trim1Element = new XmlElement("trim");
+    trim1Element.addAttribute(new Attribute("prefix", "("));
+    trim1Element.addAttribute(new Attribute("suffix", ")"));
+    trim1Element.addAttribute(new Attribute("suffixOverrides", ","));
+    for (IntrospectedColumn introspectedColumn : columns) {
+      String columnName = introspectedColumn.getActualColumnName();
+      if(!columnName.toUpperCase().equals(incrementField)){//不是自增字段的才会出现在批量插入中
+        TextElement te1 = new TextElement("`"+columnName + "`,");
+        trim1Element.addElement(te1);
+        TextElement textElement = new TextElement("#{item." + introspectedColumn.getJavaProperty() + ",jdbcType=" + introspectedColumn.getJdbcTypeName() + "},");
+        javaPropertyAndDbType.addElement(textElement);
+      }
+    }
+
+    XmlElement foreachElement = new XmlElement("foreach");
+    foreachElement.addAttribute(new Attribute("collection", "list"));
+    foreachElement.addAttribute(new Attribute("index", "index"));
+    foreachElement.addAttribute(new Attribute("item", "item"));
+    foreachElement.addAttribute(new Attribute("separator", ","));
+    insertBatchElement.addElement(new TextElement("insert into " + introspectedTable.getAliasedFullyQualifiedTableNameAtRuntime()));
+    insertBatchElement.addElement(trim1Element);
+    insertBatchElement.addElement(new TextElement(" values "));
+    foreachElement.addElement(javaPropertyAndDbType);
+    insertBatchElement.addElement(foreachElement);
+
+    document.getRootElement().addElement(insertBatchElement);
+  }
+
   @Override
   public boolean validate(List<String> warnings) {
     return true;
@@ -55,6 +101,7 @@ public class BatchInsertPlugin extends PluginAdapter{
     ibsmethod.setReturnType(ibsreturnType);
     // 3.设置方法名
     ibsmethod.setName("insertBatchSelective");
+    ibsmethod.setName("insertBatch");
     // 4.设置参数列表
     FullyQualifiedJavaType paramType = FullyQualifiedJavaType.getNewListInstance();
     FullyQualifiedJavaType paramListType;
